1f2ec6f3c35db37b58a33f150121cfe23e3abdb5
Tien Nam Dao <tien.dao2@tiki.vn>
2023-02-01 16:14:20 +0700

    fix: use host from current window.location.host instead of HOST from .env

--- src/components/Logo/CurrencyLogo.tsx
+++ src/components/Logo/CurrencyLogo.tsx
@@ -36,7 +36,9 @@ export default function CurrencyLogo({
 
 	// Fix: in case of token image not found in trustwallet cdc image
 	if (currency instanceof Token && currency.address) {
-		const image = `${process.env.NEXT_PUBLIC_HOST}/images/tokens/${currency.address}.png`
+		const currentHost = window.location.host
+
+		const image = `${currentHost}/images/tokens/${currency.address}.png`
 		if (!srcs.includes(image)) srcs.push()
 	}
 
